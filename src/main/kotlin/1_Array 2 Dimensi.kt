9886067d1d5bb2ba4525a8741697f752e6d73e4d
Yuni Zanika <yuyunisyerin@gmail.com>
2022-03-17 10:08:00 +0700

    Array 2D dikotlin

---
+++ src/main/kotlin/1_Array 2 Dimensi.kt
@@ -0,0 +1,37 @@
+fun main() {
+    /*
+    Array dua dimensi merupakan array yang terdiri dari m buah baris dan n buah kolom.
+    Bentuknya dapat berupa matriks atau tabel.
+     */
+    //initialize a 2D array
+    var cinema = arrayOf<Array<Int>>()
+    for (i in 0..4) {
+        var array = arrayOf<Int>()
+        for (j in 0..4) {
+            array += 0
+        }
+        cinema += array
+    }
+    for (array in cinema) {
+        for (value in array) {
+            print("$value ")
+        }
+        println()
+    }
+    //Filling with data
+    cinema[2][2] = 1
+    for (i in 1..3) {
+        cinema[3][i] = 1
+    }
+    for (i in 0..4) {
+        cinema[4][i] = 1
+    }
+    println()
+    for (array in cinema) {
+        for (value in array) {
+            print("$value ")
+        }
+        println()
+
+    }
+}
