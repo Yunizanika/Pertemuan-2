b4cd403d0f31ce855d1c0fe961797adcaefed1a2
Yuni Zanika <yuyunisyerin@gmail.com>
2022-03-17 09:37:20 +0700

    Array Dengan Tipe Primitive

--- src/main/kotlin/1_TemplateArray.kt
+++ src/main/kotlin/1_TemplateArray.kt
@@ -6,7 +6,7 @@ fun main() {
 
     /*
     berikut adalah contoh pembuatan array berdasarkan template array standard pada kotlin.
-    codingan diatas telah ditentuan index array yang akan ditampilkan yaitu dengan size indexnya 5, sehingga akan dilakukan penambahan 5 dari
-    0 sehingga hasilnya: 0 1 4 9 16
+    codingan diatas telah ditentuan index array yang akan ditampilkan yaitu dengan size indexnya 5, sehingga akan dilakukan penambahan
+    5 dari 0 sehingga hasilnya: 0 1 4 9 16
      */
 }
