f03ab0b0078867cf9a9e8ecf8bb5d64e375cf655
Yuni Zanika <yuyunisyerin@gmail.com>
2022-03-19 17:43:41 +0700

    break dan continue

---
+++ src/main/kotlin/5_Break Dan Continue(for loop).kt
@@ -0,0 +1,18 @@
+fun main() {
+    //Continue -> Continue digunakan saat kita ingin melewati
+    //Break -> berhenti
+    println("Example of Break and Continue Label")
+    myLabel@ for(x in 1..10) { // appling the custom label
+        if(x == 5) {
+            println("I am inside if block with value"+x+"\n-- hence it will close the operation")
+            break@myLabel // menentukan label
+        } else {
+            println("I am inside else block with value"+x)
+            continue@myLabel
+        }
+        println("Not Print")
+    }
+    /*
+
+     */
+}
