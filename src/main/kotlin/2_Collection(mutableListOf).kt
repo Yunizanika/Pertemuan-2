503318a7343f9ec5011809b8c5f6f68b954f721a
Yuni Zanika <yuyunisyerin@gmail.com>
2022-03-19 17:27:06 +0700

    MapMutable ListOf

---
+++ src/main/kotlin/2_Collection(mutableListOf).kt
@@ -0,0 +1,15 @@
+fun main() {
+    // MutableList adalah Daftar dengan operasi penulisan khusus daftar
+    val numbers = mutableListOf(1, 2, 3, 4)
+    numbers.add(5)
+    numbers.removeAt(1)
+    numbers[0] = 0
+    numbers.shuffle()
+    println(numbers)
+    val numbersStr = mutableListOf("one", "two", "three", "four")
+    numbersStr.add("five") // this is OK
+
+
+    // numbersStr = mutableListOf("six", "seven") // kesalahan kompilasi
+    println(numbersStr)
+}
