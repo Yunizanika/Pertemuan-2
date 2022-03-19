6143626ac5d5823e667785491c35dcb7f10b851e
Yuni Zanika <yuyunisyerin@gmail.com>
2022-03-19 17:25:07 +0700

    Map

---
+++ src/main/kotlin/2_Collection(Map).kt
@@ -0,0 +1,23 @@
+fun main() {
+    // Map (atau kamus) : adalah sebuah set pasangan kunci dan nilai (key-value). Kunci
+    //bersifat unik dan hanya memiliki satu nilai. Sedangkan nilai yang ada pada Map dapat
+    //bernilai ganda/sama.
+
+    val numbersMap = mapOf("key1" to 1, "key2" to 2, "key3" to 3, "key4" to 1)
+    println("All keys: ${numbersMap.keys}")
+    println("All values: ${numbersMap.values}")
+    if ("key2" in numbersMap) println("Value by key \"key2\": ${numbersMap["key2"]}")
+    if (1 in numbersMap.values) println("The value 1 is in the map")
+    if (numbersMap.containsValue(1)) println("The value 1 is in the map") // same as previous
+    //wo maps containing the equal pairs are equal regardless of the pair order.
+    val anotherMap = mapOf("key2" to 2, "key1" to 1, "key4" to 1, "key3" to 3)
+    println("The maps are equal: ${numbersMap == anotherMap}")
+    //MutableMap is a Map with map write operations, for example,
+    //you can add a new key-value pair or update the value associated with the given key
+    val mutableMap = mutableMapOf("one" to 1, "two" to 2)
+    mutableMap.put("three", 3)
+    mutableMap["one"] = 11
+    println(mutableMap)
+    val readWriteMap = hashMapOf("foo" to 1, "bar" to 2)
+    println(readWriteMap["foo"]) // prints "1"
+}
