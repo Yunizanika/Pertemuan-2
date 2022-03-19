2a968cd6da200f3062bc2bce72161615dd898bfd
Yuni Zanika <yuyunisyerin@gmail.com>
2022-03-19 17:32:05 +0700

    Ranges

---
+++ src/main/kotlin/3_Ranges.kt
@@ -0,0 +1,14 @@
+fun main() {
+    //Ranges
+    /*
+    Ranges adalah karakteristik unik dari Kotlin. Seperti python, ranges menyediakan sebuah
+operator yang dapat membantu kita dalam membuat sebuah deretan angka melalui suatu iterasi.
+     */
+    val i:Int = 2
+    for (j in 1..4)
+        print(j) // cetak "1234"
+    println()
+    if (i in 1..10) { // equivalent of 1 <= i && i <= 10
+        println("we found your number --"+i)
+    }
+}
