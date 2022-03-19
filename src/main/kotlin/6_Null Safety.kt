9f87c2b74b84fe98222834b34c92b6cd3eec3d1a
Yuni Zanika <yuyunisyerin@gmail.com>
2022-03-19 17:55:22 +0700

    Null Safety

---
+++ src/main/kotlin/6_Null Safety.kt
@@ -0,0 +1,12 @@
+fun main() {
+    /*
+    Salah satu kesalahan yang banyak ditemukan pada bahasa pemrograman adalah pengaksesan
+    terhadap anggota referensi null dan akan menghasilkan pesan kesalahan null reference exception,
+    di Java sering disebut dengan Null Pointer Exception (NPE). Kotlin type system bertujuan untuk
+    menghilangkan NPE dari kode program.
+     */
+    //Nullable types
+    var maybeNumber: Int? = 15
+    maybeNumber = null
+    println("maybeNumber: $maybeNumber ${maybeNumber?.hashCode()}")
+}
