c0b4f67de10e46b22248c44d4eae5509943b592a
Yuni Zanika <yuyunisyerin@gmail.com>
2022-03-24 09:41:08 +0700

    Break Dan Continue

--- src/main/kotlin/5_Break Dan Continue.kt
+++ src/main/kotlin/5_Break Dan Continue.kt
@@ -5,6 +5,8 @@ fun main() {
         if(x == 7) break
         if(x == 3) continue
         print("$x ")
+        // hasil nyya akan 1 2 4 5 6 karna ketika x nya ada 7 maka dia akan berhenti dan jika x ada angka 3 maka
+        // dia akan melanjutkan ke angka selanjutnya
     }
     print('\n')
 }
